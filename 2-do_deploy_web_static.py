e36345df8e91c9cc97c5d19dd5df38cc0cbd730e
WambuaJoe <kaytujohn254@gmail.com>
2024-03-25 10:28:14 +0300

    code update - fix recurring error

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
 """
-distributes an archive to web serves using do_deploy function
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
 """
 
-from fabric.api import *
+from fabric.api import put, run, env
 from os.path import exists
-env.hosts = ['<IP web-01>', '<IP web-02>']
-env.user = 'ubuntu'
+env.hosts = ['142.44.167.228', '144.217.246.195']
 
 
 def do_deploy(archive_path):
-    """distributing function"""
-    if not exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
     try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        archive_name = archive_path.split('/')[-1]
-        folder_name = archive_name.split('.')[0]
-        run('mkdir -p /data/web_static/releases/{}'.format(folder_name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-            .format(archive_name, folder_name))
-        run('rm /tmp/{}'.format(archive_name))
-        run('mv /data/web_static/releases/{}/web_static/* '
-            '/data/web_static/releases/{}/'
-            .format(folder_name, folder_name))
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(folder_name))
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(folder_name))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception as error:
+    except Exception:
         return False
